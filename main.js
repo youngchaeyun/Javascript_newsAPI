079735e4c645bebbd9e45805bd60e513f92a7cd5
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-10-06 19:59:59 +0900

    apiKey 완료, input 값 얻기

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const apiKey = '88003d343a00410086103314ff969995';
+
+document.getElementById('searchBtn').addEventListener('click', searchNews)
+
+function searchNews() {
+    let input = document.getElementById('searchInput').value
+    let url = ``
+}
