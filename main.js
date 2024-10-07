43ac51c2f79ba34f6c6f585fcf8d82c50398a914
{youngchaeyun} <{https://github.com/youngchaeyun/Javascript_newsAPI}>
2024-10-07 11:49:08 +0900

    searchNews() 함수 추가

--- main.js
+++ main.js
@@ -6,5 +6,14 @@ document.getElementById('searchBtn').addEventListener('click', searchNews)
 
 function searchNews() {
     let input = document.getElementById('searchInput').value
-    let url = ``
+    let url = `https://newsapi.org/v2/everything?q=${input}&from=2024-10-06&to=2023-10-06&sortBy=popularity&apiKey=88003d343a00410086103314ff969995`
 }
+
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         displayResults(data.ariticles)
+//     })
+//     .catch(error => {
+//         console.error('Error:', error)
+//     })
